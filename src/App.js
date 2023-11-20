2521749100f582c39265aae80ff4d9e77252c70e
dokyeong99 <dlehrud451@gmail.com>
2023-11-20 10:16:43 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './styles/common/Reset.css'
-import {Mobile, PC} from "./components/config/Responsive";
+import { Mobile, PC } from "./components/config/Responsive";
 import classes from "./styles/pages/Main.module.css";
 import Login from "./components/pages/Login/Login";
 import React from 'react';
@@ -8,18 +8,18 @@ import React from 'react';
 
 function App() {
   return (
-      <div id='wrap'>
-        <main>
-          <PC>
-            <div className={classes.pcWrap} >
-              <p className={classes.pcWrapInner}>화면을 550px 이하로 줄여 주세요.</p>
-            </div>
-          </PC>
-          <Mobile>
-            <Login />
-          </Mobile>
-        </main>
-      </div>
+    <div id='wrap'>
+      <main>
+        <PC>
+          <div className={classes.pcWrap} >
+            <p className={classes.pcWrapInner}>화면을 550pasdfx 이하로 줄여 주세요.</p>
+          </div>
+        </PC>
+        <Mobile>
+          <Login />
+        </Mobile>
+      </main>
+    </div>
 
   );
 }
